46bde4968f0534768b42a5621cac3cbb0cb72ac7
deadnews <deadnewsgit@gmail.com>
2026-08-22 01:23:14 +0700

    fix(cbz-repack): keep pages `cjxl` failed to convert

--- src/cbz-repack.zsh
+++ src/cbz-repack.zsh
@@ -2,27 +2,27 @@
 set -euo pipefail
 
 # Usage:
-# fd -e cbz --exec-batch ~/git/shell-scripts/src/cbz-repack.zsh
+# fd -e cbz --changed-before 2d --exec-batch ~/git/shell-scripts/src/cbz-repack.zsh
 
 for F in "$@"; do
     mime=$(file -b --mime-type "${F}")
 
     if [[ ${mime} == "application/zip" ]]; then
-        size=$(du -sh "${F}" | cut -f1)
         tmp=$(mktemp -d)
 
-        if unzip -jqo "${F}" -d ${tmp}; then
-            fd . ${tmp} -e jpg -e jpeg -x cjxl -d 0 --lossless_jpeg=1 {} {.}.jxl
-            fd . ${tmp} -e jpg -e jpeg -x rm -f
-            fd . ${tmp} -e png -x cjxl -d 0 {} {.}.jxl
-            fd . ${tmp} -e png -x rm -f
-            # fd . ${tmp} -e webp -x cwebp -quiet -m 6 -pass 5 -q 99 {} -o {.}.webp
-            bsdtar --format=zip -cf "${F:r}.cbz" -C ${tmp} .
-            echo ${size} → $(du -sh "${F}")
-        else
+        if ! unzip -jqo "${F}" -d ${tmp}; then
             echo unzip failed for "${F}"
-        fi
+        elif fd --has-results . ${tmp} -e jpg -e jpeg -e png; then
+            echo "$(du -sh "${F}") →"
 
+            fd . ${tmp} -e jpg -e jpeg -e png \
+                -j 8 -x cjxl --quiet -d 0 --lossless_jpeg=1 {} {.}.jxl \
+                || echo cjxl failed for some pages of "${F}"
+            fd . ${tmp} -e jxl -x rm -f {.}.jpg {.}.jpeg {.}.png
+            bsdtar --format=zip -cf "${F:r}.cbz" -C ${tmp} .
+
+            echo "$(du -sh "${F}")\n"
+        fi
         rm -rf ${tmp}
     fi
 done
